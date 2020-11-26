4a7742a70151739dcea34b8003c7641222d208f7
chau-doan <74688464+chau-doan@users.noreply.github.com>
2020-11-26 00:13:41 -0800

    Add files via upload
    Em xem thu xem bai cua chi vay dc ko

--- vertec.py
+++ vertec.py
@@ -1,23 +1,25 @@
+import heapq
+
 class vertex:
     def __init__(self, name):
         self.name = str(name)
-        self.adjacent = []                                          #[name of adjacent][weight][Is visited]
+        self.adjacent = []                                          #[name of adjacent][weight]
         self.distance = 9999999
+        self.isvisit = False
+        self.previous = None
 
-    def append_adjacent(self, name, distance):
+    def append_adjacent(self, node, distance):
         for i in range(len(self.adjacent)):
-            if self.adjacent[i][0] == name:
+            if self.adjacent[i][0] == node:
                 print("Vertex already existed.\n")
                 return
-        self.adjacent.append([str(name), float(distance), False])
-        self.sort()
+        self.adjacent.append([node, float(distance)])
 
-    def get_weight(self, name):
+    def get_weight(self, node):
         for i in range(len(self.adjacent)):
-            if self.adjacent[i][0] == name:
+            if self.adjacent[i][0] == node:
                 return self.adjacent[i][1]                          #return adjecent distance
-            else:
-                return 0                                            #not exist
+        return 0                                            #not exist
 
     def sort(self):                                                 #Insertion sort
         for i in range(1, len(self.adjacent)):
@@ -36,12 +38,10 @@ class vertex:
         return False
 
     def reset_status(self):
-        for i in range(0, len(self.adjacent)):
-            self.adjacent[i][2] = False
+        self.isvisit = False
 
-    def visit_vertex(self, number):
-        self.adjacent[number][2] = True
-        return [self.adjacent[number][0], self.adjacent[number][1]]         #return array with name and weight
+    def visit_vertex(self):
+        self.isvisit = True
 
     def set_distance(self, distance):
         self.distance = distance
@@ -54,3 +54,111 @@ class vertex:
 
     def get_name(self):
         return self.name
+
+    def set_previous(self, prev):
+        self.previous = prev
+
+    def is_visit(self):
+        return self.isvisit
+
+    def __lt__(self, other):
+        pass
+
+    def __le__(self, other):
+        pass
+
+class graph:
+    def __init__(self):
+        self.array = []
+        self.path = []
+
+    def add_vertex(self, vert):
+        self.array.append(vert)
+
+    def connect_2_direction(self, first: vertex, second: vertex, distance: float):
+        first.append_adjacent(second, distance)
+        second.append_adjacent(first, distance)
+
+    def connect_1_direction(self, first: vertex, second: vertex, distance):
+        first.append_adjacent(second, distance)
+
+    def is_connect(self, first: vertex, second: vertex) -> bool:
+        for i in range(0, len(self.array)):
+            if self.array[i] == first:
+                return self.array[i].is_connect(second)
+
+    def get_weight(self, first: vertex, second: vertex) -> float:
+        return first.get_weight(second)
+
+    def shortest(self,target, path):
+        path.append(target)
+        if target.previous:
+            self.shortest(target.previous, path)
+        return
+
+    def dijkstra(self, start: vertex):
+        start.set_distance(0)
+        unvisited_queue = [(int(v.get_distance()), v) for v in self.array]
+        heapq.heapify(unvisited_queue)
+        while len(unvisited_queue):
+            uv = heapq.heappop(unvisited_queue)
+            current = uv[1]
+            current.visit_vertex()
+            for next in current.adjacent:
+                if next[0].is_visit():
+                    continue
+                new_dist = current.get_distance() + current.get_weight(next)
+
+                if new_dist < next[0].get_distance():
+                    next[0].set_distance(new_dist)
+                    next[0].set_previous(current)
+                else:
+                    pass
+
+            while len(unvisited_queue):
+                heapq.heappop(unvisited_queue)
+            unvisited_queue = [(int(v.get_distance()), v) for v in self.array if not v.isvisit]
+            heapq.heapify(unvisited_queue)
+
+if __name__ == "__main__":
+    a = vertex("A")
+    b = vertex("B")
+    c = vertex("C")
+    d = vertex("D")
+    e = vertex("E")
+    f = vertex("F")
+    g = vertex("G")
+    h = vertex("H")
+
+    test_graph = graph()
+    test_graph.add_vertex(a)
+    test_graph.add_vertex(b)
+    test_graph.add_vertex(c)
+    test_graph.add_vertex(d)
+    test_graph.add_vertex(e)
+    test_graph.add_vertex(f)
+    test_graph.add_vertex(g)
+    test_graph.add_vertex(h)
+
+    test_graph.connect_2_direction(a, b, 10)
+    test_graph.connect_2_direction(a, c, 3)
+    test_graph.connect_2_direction(a, d, 8)
+    test_graph.connect_2_direction(b, c, 8)
+    test_graph.connect_2_direction(c, d, 4)
+    test_graph.connect_2_direction(b, e, 6)
+    test_graph.connect_2_direction(c, f, 9)
+    test_graph.connect_2_direction(d, g, 7)
+    test_graph.connect_2_direction(e, f, 3)
+    test_graph.connect_2_direction(f, g, 1)
+    test_graph.connect_2_direction(e, h, 11)
+    test_graph.connect_2_direction(f, h, 8)
+    test_graph.connect_2_direction(g, h, 5)
+
+    print(test_graph.is_connect(a, d))
+    print(test_graph.get_weight(a, c))
+    test_graph.dijkstra(a)
+    path = []
+    test_graph.dijkstra(a)
+    test_graph.shortest(h, path)
+    for i in range(0, len(path)):
+        print(path[i].name)
